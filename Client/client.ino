b48c7ec479e496d1d51d90fcbf10b902dec721eb
Cristian Flores <cristian.flores@kushkipagos.com>
2023-05-20 07:40:34 -0500

    finishing client source code

--- Client/client.ino
+++ Client/client.ino
@@ -5,16 +5,23 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid = "NextCRSnet";
+const char* thisIdDevice = "SDM-001";
+const char* ssid = "FIBRAMAX_CRISTIAN";
 const char* password = "1725crs19";
+const char* serverName = "http://192.168.123.102:3022/contacts/init";
+const char* thisDeviceNameCode = "SDM-";
+const char* JWT = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY0NjhhZThmYWI0NGM1YzYzZjQ2ZjdiOSIsImZ1bGxOYW1lIjoiRGV2aWNlQWNjb3VudCIsImlkRGV2aWNlIjoiaWREZXZpY2UiLCJpc0RldmljZSI6dHJ1ZSwiaXNQb3NzaWJsZVNpY2siOmZhbHNlLCJpc1NpY2siOmZhbHNlLCJtYWlsIjoiZGV2aWNlQGEuY29tIiwicm9sIjoiREVWSUNFIiwibmVhck5vZGVzIjpbXSwiX192IjowfSwiaWF0IjoxNjg0NTgyNDA1LCJleHAiOjE2ODQ2Njg4MDV9.UpoOtLvVO7VxiTs4yW7vNi6ZObVM7t0ujlTPdNOFKzw";
+const double A = -66.5;
+const double n = 2.6;
+bool devicesMemory[200] = {0};
 
-const char* serverName = "http://192.168.101.2:3022/contacts/init";
+const int SOUND_PIN = 23;
 
-const int PIN = 2;
-const int CUTOFF = -60;
+const int DISTANCE_LIMIT = 1.5;
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Soy el dispositivo cliente " + String(thisIdDevice));
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
@@ -25,11 +32,8 @@ void setup() {
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
- 
-  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
-  
-  pinMode(PIN, OUTPUT);
+  Serial.println(WiFi.localIP()); 
+  pinMode(SOUND_PIN, OUTPUT);
   BLEDevice::init("");
 }
 
@@ -40,38 +44,150 @@ void loop() {
   BLEScan *scan = BLEDevice::getScan();
   scan->setActiveScan(true);
   BLEScanResults results = scan->start(1);
-  int best = CUTOFF;
+  Serial.println("********************* Count: " + String(results.getCount()) + " ************************");  
+  Serial.println(results.getCount());
+
   for (int i = 0; i < results.getCount(); i++) {
     BLEAdvertisedDevice device = results.getDevice(i);
-    Serial.println(results.getCount());
-    char *a = "pepe";
-    Serial.println("que jesto?");
-    Serial.println(a);
-    Serial.println(device.getName().c_str());
-    doc["idDevice"] = device.getName().c_str();
+    const char* contactDeviceName = device.getName().c_str();
+    char codeContactDeviceName[4];
     int rssi = device.getRSSI();
-    if (rssi > best) {
-      best = rssi;
-
-      WiFiClient client;
-      HTTPClient http;
+    int isTheSameDevice = strcmp(thisIdDevice, contactDeviceName);
+    strncpy(codeContactDeviceName, contactDeviceName, 4);
+    codeContactDeviceName[4] = '\0';
+    int isTheSameCode = strcmp(thisDeviceNameCode, codeContactDeviceName);
     
-      // Your Domain name with URL path or IP address with path
-      http.begin(client, serverName);
+    double distance = calculateDistance(rssi);
+    
+    Serial.print(i+1);
+    Serial.print(": ");
+    Serial.println(device.getName().c_str());
+    Serial.print("RSSI: ");
+    Serial.print(rssi);
+    Serial.print(" -> Distance: ");
+    Serial.println(distance);
+    Serial.println("Es el mismo dispositivo -> " + String(isTheSameDevice));
 
-      serializeJsonPretty(doc, Serial);
-      String json;
-      serializeJson(doc,json);
+    
+
+    if (isTheSameDevice != 0 && isTheSameCode == 0) {
+      char numberContactDeviceName[4];
+      substring(contactDeviceName, 4, 6, numberContactDeviceName);
+      int numberContactDeviceNameInt = atoi(numberContactDeviceName);
+      Serial.println("numberContactDeviceName: " + String(numberContactDeviceName));
+      Serial.println("number transformed to int -> " + String(numberContactDeviceNameInt));
+      Serial.print("devicesMemory[numberContactDeviceNameInt] -> ");
+      Serial.println(devicesMemory[numberContactDeviceNameInt]);
+
+      if(distance < DISTANCE_LIMIT) {
+        Serial.println("------------------------------------- DISTANCIA MENOR A 1.5m -----------------------------------------------");
+        
+        if (devicesMemory[numberContactDeviceNameInt] == 1) {
+          Serial.println("Dispisitivo ya registrado");
+          Serial.println("[STEP]> Emitiendo alarma sonora");
+          emitTwoBeeps();
+
+        } else {
+          Serial.println("[STEP]> Emitiendo alarma sonora");
+          emitTwoBeeps();
+          Serial.println("[STEP]> Enviar datos via POST init TRUE");
+        
+          Serial.println("Este si esta dentro del rango");
+          WiFiClient client;
+          HTTPClient http;
+
+          Serial.print("[PROCESSING]: ");
+          Serial.println(device.getName().c_str());
+          doc["idDevice"] = String(thisIdDevice);
+          doc["idContactDevice"] = String(device.getName().c_str());
+          doc["rssi"] = String(rssi);
+          doc["isInit"] = true;
       
-      http.addHeader("Content-Type", "application/json");
-      int httpResponseCode = http.POST(json);
+          http.begin(client, serverName);
+
+          serializeJsonPretty(doc, Serial);
+          Serial.println();
+          String json;
+          serializeJson(doc,json);
+          
+          http.addHeader("Content-Type", "application/json");
+          http.addHeader("Authorization", String(JWT));
+          int httpResponseCode = http.POST(json);
+        
+          Serial.print("HTTP Response code: ");
+          Serial.println(httpResponseCode);
+
+          Serial.println("[STEP]> Guardar en memoria");
+          devicesMemory[numberContactDeviceNameInt] = 1;
+        } 
      
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
+      } else {
+        Serial.println("------------------------------------- DISTANCIA MAYOR A 1.5m -----------------------------------------------");
+        
+        if (devicesMemory[numberContactDeviceNameInt] == 1) {
+          Serial.println("[STEP]> Enviar datos via POST init FALSE");
+        
+          Serial.println("Este si esta dentro del rango");
+          WiFiClient client;
+          HTTPClient http;
+
+          Serial.print("[PROCESSING]: ");
+          Serial.println(device.getName().c_str());
+          doc["idDevice"] = String(thisIdDevice);
+          doc["idContactDevice"] = String(device.getName().c_str());
+          doc["rssi"] = String(rssi);
+          doc["isInit"] = false;
       
+          http.begin(client, serverName);
+
+          serializeJsonPretty(doc, Serial);
+          Serial.println();
+          String json;
+          serializeJson(doc,json);
+          
+          http.addHeader("Content-Type", "application/json");
+          http.addHeader("Authorization", String(JWT));
+          int httpResponseCode = http.POST(json);
+        
+          Serial.print("HTTP Response code: ");
+          Serial.println(httpResponseCode);
+
+          Serial.println("[STEP]> Eliminar dispositivo en memoria");
+          devicesMemory[numberContactDeviceNameInt] = 0;
+        }
+      }
     }
-    Serial.println(rssi);
-    delay(4000);
   }
-  digitalWrite(PIN, best > CUTOFF ? HIGH : LOW);
+  delay(4000);
+}
+
+void emitTwoBeeps() {
+  tone(SOUND_PIN, 1000);  
+  delay(100);             
+  noTone(SOUND_PIN);      
+  
+  delay(100);             
+  
+  tone(SOUND_PIN, 1000);  
+  delay(100);             
+  noTone(SOUND_PIN);      
+}
+
+void substring(const char* str, int start, int length, char* result) {
+    const char* sourcePtr = str + start;
+    char* destinationPtr = result;
+    
+    for (int i = 0; i < length; i++) {
+        *destinationPtr = *sourcePtr;
+        sourcePtr++;
+        destinationPtr++;
+    }
+    
+    *destinationPtr = '\0';
+}
+
+double calculateDistance(int rssi) {
+  double distance = pow(10, - ((rssi - A) / (10 * n)));
+
+  return distance;
 }
--- server/server.ino
+++ server/server.ino
@@ -14,9 +14,11 @@
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
+const char* idDevice = "SDM-001";
+
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting BLE work!");
+  Serial.println("Hola soy el dispositivo servidor " + String(idDevice));
 
   BLEDevice::init("Long name works now");
   BLEServer *pServer = BLEDevice::createServer();
